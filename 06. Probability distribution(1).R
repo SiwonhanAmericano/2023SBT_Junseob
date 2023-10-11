c3b51bb3dd8b173509853476bc7b419a54485a21
Junseob <bokhung19@hanyang.ac.kr>
2023-10-11 16:23:02 +0900

    Week6 script 1

---
+++ 06. Probability distribution(1).R
@@ -0,0 +1,19 @@
+#Week6 script
+#이항 분포
+
+dbinom(3,3,0.5)
+
+dbinom(9,10,0.99)+dbinom(10,10,0.99)
+
+pbinom(1,1000,0.01)
+
+1-pbinom(91,100,0.8)
+
+#포아송 분포
+
+dpois(2,1.2)
+
+dpois(41,45)
+ppois(41,45)-ppois(40,45)
+
+1-ppois(40,45)
